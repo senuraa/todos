08151b561e034790146e2e15c2b8c91035a84ad5
Senura Seneviratne <senuraa@msn.com>
2018-06-19 19:51:38 +0530

    remove unused imports

--- app/src/pages/login/login.module.ts
+++ app/src/pages/login/login.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
-import {CountryPickerService} from 'ngx-country-picker'
 @NgModule({
   declarations: [
     LoginPage,
