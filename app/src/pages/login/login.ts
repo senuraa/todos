565fb1a470817d77cbaf4958f5734b003bb587ed
Senura Seneviratne <senuraa@msn.com>
2018-06-19 15:21:53 +0530

    login ui completed

--- app/src/pages/login/login.ts
+++ app/src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,12 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  loginInit:boolean = true
+  loginDetEnter:boolean=false
+  loginForm:FormGroup;
   subTitle: string = 'Create a Todos account and add your team members'
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder:FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      first_name: ['',Validators.required],
+      last_name: ['',Validators.required],
+      phone_number: ['',Validators.compose([Validators.required,Validators.minLength(10),Validators.maxLength(10)])],
+    })
   }
 
   onInitButtonClick() {
-    
+    this.loginInit=false
+    this.loginDetEnter=true
+    this.subTitle='Let\'s get started'
   }
 }
