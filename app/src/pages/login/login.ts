21a273ce93b3aaa551db216fbaea9a41aab6c017
Senura Seneviratne <senuraa@msn.com>
2018-06-19 21:12:12 +0530

    signup completed

--- app/src/pages/login/login.ts
+++ app/src/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { CountryPickerService, ICountry } from 'ngx-country-picker'
+import { CountryPickerService, ICountry } from 'ngx-country-picker';
+import { AuthService } from '../../providers/auth.service'
 /**
  * Generated class for the LoginPage page.
  *
@@ -21,20 +22,28 @@ export class LoginPage {
   subTitle: string = 'Create a Todos account and add your team members'
   country: any;
   countries: ICountry[] = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public countryPickerService: CountryPickerService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public countryPickerService: CountryPickerService, public authService: AuthService) {
     this.countryPickerService.getCountries().subscribe((countries: ICountry[]) => //get all country
       this.countries = countries);  // store it in countries
     this.loginForm = this.formBuilder.group({
       first_name: ['', Validators.required],
       last_name: ['', Validators.required],
       phone_number: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])],
-      country:['94']
+      country_code: ['94']
     })
   }
 
-  login(user){
+  login(user) {
     console.log(user)
-    this.navCtrl.push('VerifyPage',user)
+    //this.navCtrl.push('VerifyPage', user)
+    this.authService.receiveSMS(user).then((response)=>{
+      console.log(response)
+      this.navCtrl.push('VerifyPage', user)
+    },(err)=>{
+      console.log(err)
+    }
+  )
+    
   }
   onInitButtonClick() {
     this.loginInit = false
--- app/src/pages/verify/verify.ts
+++ app/src/pages/verify/verify.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AuthService } from '../../providers/auth.service';
 /**
  * Generated class for the VerifyPage page.
  *
@@ -15,9 +15,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class VerifyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authService:AuthService) {
 
+  }
+  user: any = {
+    first_name: this.navParams.get('first_name'),
+    last_name: this.navParams.get('last_name'),
+    phone_number: this.navParams.get('phone_number'),
+    country_code: this.navParams.get('country_code'),
+    token: ''
+  }
+  verify(){
+    this.authService.verifyToken(this.user).then((response)=>{
+      console.log(response)
+      window.localStorage.setItem('phone_number', this.user.phone_number);
+      this.navCtrl.push('TabsPage');
+    },(err)=>{
+      console.log(err)
+    })
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad VerifyPage');
   }
