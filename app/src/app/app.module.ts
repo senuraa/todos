e840dd78f4705055c2fbff94ab4df0f101271255
Senura Seneviratne <senuraa@msn.com>
2018-06-19 19:43:34 +0530

    added country picker,login

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -8,13 +8,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthService } from '../providers/auth.service';
 import { TasksService } from '../providers/tasks.service';
 
+import {CountryPickerModule} from 'ngx-country-picker'
+
+
 @NgModule({
   declarations: [
     MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CountryPickerModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -23,9 +27,10 @@ import { TasksService } from '../providers/tasks.service';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     AuthService,
     TasksService
-  ]
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule { }
